f017202fe1d331081aa866c49cb929bad49d224c
abel <abel@192.168.0.12>
2018-09-17 11:02:58 +0530

    gferte

--- spring/src/com/java/bluedart.java
+++ spring/src/com/java/bluedart.java
@@ -4,7 +4,7 @@ public class bluedart implements courier {
 
 	@Override
 	public void deliver(int orderid) {
-	System.out.println("bluedart is ready to  deliver");
+	System.out.println("blxcfsfsadfsdafdsasdfauedart is ready to  deliver");
 		
 	}
 
